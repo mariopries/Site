883eb984f7e60bf8424ee83290906fe96df1650c
Douglas dos Santos <douglasdossantos22@gmail.com>
2017-01-21 23:42:39 -0200

    configuração painel
    Alterado "BaseURI", para 'site/painel', antes era só 'painel'

--- painel/config/application.php
+++ painel/config/application.php
@@ -8,7 +8,7 @@ return array(
 	'ConquerCash'         => 'cash',
 	//Ragnarok
 	'ServerAddress'        => 'localhost',              // This value is the hostname:port under which Flux runs. (e.g., example.com or example.com:80)
-	'BaseURI'              => 'painel',                 // The base URI is the base web root on which your application lies.
+	'BaseURI'              => 'site/painel',                 // The base URI is the base web root on which your application lies.
 	'InstallerPassword'    => 'kBMjARB4fb94K8kaabY5Bv8rktQmH8GqGUPaEjNJqDF3rymEg3tX6qBHdPPnzSvC',         // Installer/updater password.
 	'RequireOwnership'     => true,                     // Require the executing user to be owner of the FLUX_ROOT/data/ directory tree? (Better for security)
 	                                                    // WARNING: This will be mostly IGNORED on non-POSIX-compliant OSes (e.g. Windows).
